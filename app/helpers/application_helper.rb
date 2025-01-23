81ad8a293f322c9d1b5a80863dd55453f43d49a6
Emanuelvss13 <emanuelsouza2002@gmail.com>
2025-01-23 14:28:28 -0300

    feat: environment helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def format_date_to_braziliam_format(date)
     date.strftime("%d/%m/%Y")
   end
+
+  def environment
+    case Rails.env
+    when "development"
+      "Desenvolvimento"
+    when "test"
+      "Teste"
+    when "production"
+      "Produção"
+    else
+      "Não definido"
+    end
+  end
 end
